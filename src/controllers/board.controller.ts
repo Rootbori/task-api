31c1251f5a3344cc16e639d32c23ede9ba3c48dd
root <rootbeer.tiam@gmail.com>
2023-07-07 03:00:52 +0700

    up

--- src/controllers/board.controller.ts
+++ src/controllers/board.controller.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from 'express'
-import { Op } from 'sequelize'
-// @ts-ignore
-import boards from '../auto-models/boards'
+
+const Sequelize = require('sequelize')
+import sequelize from '../database'
+
+const BoardModel = require('../auto-models/boards.js')
+const Board = BoardModel(sequelize, Sequelize);
 
 export const getBoards = async (req: Request, res: Response) => {
     try {
-        const resData = await boards.findAll()
-        res.json(resData)
+        const datas = await Board.findAll()
+        res.json(datas)
     } catch (error) {
-        res.status(500).json({ error: 'Error retrieving data' })
+        console.error('Error retrieving users:', error)
+        res.status(500).json({ error: 'Error retrieving users' })
     }
 }
