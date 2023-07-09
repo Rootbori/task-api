85a5829486fd3e80d2e7d32fcefcb822d58cb1e6
root <rootbeer.tiam@gmail.com>
2023-07-10 01:01:50 +0700

    up

--- src/auto-models/init-models.js
+++ src/auto-models/init-models.js
@@ -3,6 +3,7 @@ var _boards = require("./boards");
 var _cards = require("./cards");
 var _comments = require("./comments");
 var _lists = require("./lists");
+var _tasks = require("./tasks");
 var _users = require("./users");
 
 function initModels(sequelize) {
@@ -10,6 +11,7 @@ function initModels(sequelize) {
   var cards = _cards(sequelize, DataTypes);
   var comments = _comments(sequelize, DataTypes);
   var lists = _lists(sequelize, DataTypes);
+  var tasks = _tasks(sequelize, DataTypes);
   var users = _users(sequelize, DataTypes);
 
   lists.belongsTo(boards, { as: "board", foreignKey: "board_id"});
@@ -18,12 +20,15 @@ function initModels(sequelize) {
   cards.hasMany(comments, { as: "comments", foreignKey: "card_id"});
   cards.belongsTo(lists, { as: "list", foreignKey: "list_id"});
   lists.hasMany(cards, { as: "cards", foreignKey: "list_id"});
+  tasks.belongsTo(lists, { as: "list", foreignKey: "list_id"});
+  lists.hasMany(tasks, { as: "tasks", foreignKey: "list_id"});
 
   return {
     boards,
     cards,
     comments,
     lists,
+    tasks,
     users,
   };
 }
--- src/auto-models/tasks.js
+++ src/auto-models/tasks.js
@@ -1,27 +1,39 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('boards', {
-    board_id: {
+  return sequelize.define('tasks', {
+    task_id: {
       autoIncrement: true,
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true
     },
-    board_name: {
+    list_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'lists',
+        key: 'list_id'
+      }
+    },
+    task_name: {
       type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    task_order: {
+      type: DataTypes.INTEGER,
       allowNull: true
     }
   }, {
     sequelize,
-    tableName: 'boards',
+    tableName: 'tasks',
     schema: 'public',
     timestamps: false,
     indexes: [
       {
-        name: "boards_pkey",
+        name: "lists_pkey_1",
         unique: true,
         fields: [
-          { name: "board_id" },
+          { name: "task_id" },
         ]
       },
     ]
