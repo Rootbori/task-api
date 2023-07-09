a8f165d591da3a199d5fc2a1a88a42b087af0f5a
root <rootbeer.tiam@gmail.com>
2023-07-10 00:51:36 +0700

    up

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,9 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-const port = 3001;
 
-app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
 });
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,22 @@
+
 // @ts-ignore
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express';
 import { getUsers, searchUsers } from './controllers/user.controller'
 import { addBoard, getBoards } from './controllers/board.controller'
 import { Login } from './controllers/users.controller'
 
-const router = express.Router()
+const router = express.Router();
+
+router.use(function(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 router.get('/task/users', getUsers)
 router.post('/task/searchUsers', searchUsers)
-router.get('/task/login', Login)
+router.post('/task/login', Login)
 
 router.get('/task/searchBoards', getBoards)
 router.post('/task/addBoards', addBoard)
